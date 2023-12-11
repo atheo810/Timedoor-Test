1d2a547874001b84172138003493cc3e6f7de2af
atheo810 <atheos810@gmail.com>
2023-12-11 17:07:13 +0800

    feat: unique

--- database/migrations/2023_12_11_060100_create_authors_table.php
+++ database/migrations/2023_12_11_060100_create_authors_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('authors', function (Blueprint $table) {
             $table->id();
-            $table->string("author_name");
+            $table->string("author_name")->unique();
             $table->timestamps();
         });
     }
