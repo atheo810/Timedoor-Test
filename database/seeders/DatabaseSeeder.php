f5f48c465d28c6a0f0295f6b2c3d8caa92c285e9
atheo810 <atheos810@gmail.com>
2023-12-11 14:12:27 +0800

    feat: seed factory author

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\author;
+use Database\Factories\AuthorFactory;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -18,5 +21,7 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        author::factory(1000)->create();
     }
 }
