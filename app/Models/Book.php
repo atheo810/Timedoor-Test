7214c72ac231955f15da764404ceeaedd1e0d82b
atheo810 <atheos810@gmail.com>
2023-12-11 15:20:28 +0800

    feat: Book Models

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -8,4 +8,22 @@ use Illuminate\Database\Eloquent\Model;
 class Book extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'book_name',
+        'book_category_id',
+        'author_id'
+    ];
+
+    protected $table = 'books';
+
+    public function category()
+    {
+        return $this->belongsTo(BookCategory::class, 'book_category_id');
+    }
+
+    public function author()
+    {
+        return $this->belongsTo(Author::class);
+    }
 }
