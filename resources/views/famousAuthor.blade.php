f6687fdebc771df6fb951bc3e78fe5a6eec80a85
atheo810 <atheos810@gmail.com>
2023-12-11 22:20:28 +0800

    feat: view top author

--- resources/views/famousAuthor.blade.php
+++ resources/views/famousAuthor.blade.php
@@ -1,5 +1,22 @@
 @include('components.header')
 
+<table>
+    <tr>
+        <td>No</td>
+        <td>Author Name</td>
+        <td>Voter</td>
+    </tr>
+    @php
+        $i = 1;
+    @endphp
+    @foreach ($authors as $author)
+        <tr>
+            <td>{{ $i++ }}</td>
+            <td>{{ $author->author_name }}</td>
+            <td>Voter</td>
 
+        </tr>
+    @endforeach
+</table>
 
 @include('components.footer')
