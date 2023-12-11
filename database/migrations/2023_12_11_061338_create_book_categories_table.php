75dc67cdc7ee4968b28c20323206008d6754479c
atheo810 <atheos810@gmail.com>
2023-12-11 17:07:26 +0800

    feat: unique category

--- database/migrations/2023_12_11_061338_create_book_categories_table.php
+++ database/migrations/2023_12_11_061338_create_book_categories_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('book_categories', function (Blueprint $table) {
             $table->id();
-            $table->string("category_name");
+            $table->string("category_name")->unique();
             $table->timestamps();
         });
     }
